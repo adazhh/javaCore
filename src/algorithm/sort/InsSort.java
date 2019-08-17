6d81fe14b5588e915ae08d1bc0825d72770682c3
matebookUbuntu <ada.matebookUbuntu.com>
2019-08-17 19:49:42 +0800

    20190817 19:49

--- src/algorithm/sort/InsSort.java
+++ src/algorithm/sort/InsSort.java
@@ -1,5 +1,20 @@
 package algorithm.sort;
 
-public class InsSort {
+import java.util.Arrays;
 
+public class InsSort {
+    public void insSort01(int[] arr){
+        for (int i=1; i<arr.length; i++){
+            int temp = arr[i];
+            for (int j=i-1; j>=0; j--){
+                if (temp < arr[j]){
+                    arr[j+1] = arr[j];
+                }else {
+                    arr[j+1] = temp;
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
---
+++ src/algorithm/sort/sMain.java
@@ -0,0 +1,10 @@
+package algorithm.sort;
+
+public class sMain {
+    public static void main(String[] args) {
+        InsSort insSort = new InsSort();
+        int[] arr = new int[]{3,6,1,2,8};
+        insSort.insSort01(arr);
+
+    }
+}
