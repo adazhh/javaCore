2070f8dbacba98cd8963924343db77746c64e058
matebookUbuntu <ada.matebookUbuntu.com>
2019-08-20 15:39:21 +0800

    routine commit

--- src/algorithm/sort/sMain.java
+++ src/algorithm/sort/sMain.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 
 public class sMain {
     public static void main(String[] args) {
-        int[] arr = new int[]{28,4,23,45,3,33,7,55,12,9,19,16,2,10,8};
+        int[] arr = new int[]{28,4,23,45,12,3,33,7,55,12,9,19,16,2,10,8};
         // 插入排序
         InsSort.insSort01(arr.clone());
         // 选择排序
         SelectionSort.sort(arr.clone());
         // 冒泡排序
         BubSort.sort(arr.clone());
+        // 快速排序
+        QuickSort.sort(arr.clone());
+        // 归并排序
+        MergeSort.sort(arr.clone());
 
 
     }
