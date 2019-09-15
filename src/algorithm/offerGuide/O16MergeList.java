597bd1603914bdec4400f621797b328bb2465c09
matebookUbuntu <ada.matebookUbuntu.com>
2019-09-15 22:05:07 +0800

    update

--- src/algorithm/offerGuide/O16MergeList.java
+++ src/algorithm/offerGuide/O16MergeList.java
@@ -8,10 +8,32 @@ public class O16MergeList {
         if (list1 == null) return list2;
         if (list2 == null) return list1;
         ListNode mergeHead = null;
+        ListNode current = null;
         while (list1!=null && list2!=null){
-            
+            if (list1.val <= list2.val){
+                if (mergeHead == null){
+                    mergeHead = current = list1;
+                }else {
+                    current.next = list1;
+                    current = current.next;
+                }
+                list1 = list1.next;
+            }else {
+                if (mergeHead == null){
+                    mergeHead = current = list2;
+                }else {
+                    current.next = list2;
+                    current = current.next;
+                }
+                list2 = list2.next;
+            }
         }
-        return null;
+        if (list1 != null) {
+            current.next = list1;
+        }else {
+            current.next = list2;
+        }
+        return mergeHead;
     }
     public class ListNode {
         int val;
