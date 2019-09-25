104762e9b47f6945e7d3d03c4d428f531b92bc1c
matebookUbuntu <ada.matebookUbuntu.com>
2019-09-25 10:01:55 +0800

    update

--- src/test.java
+++ src/test.java
@@ -1,11 +1,27 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>(100);
-        System.out.println(list.size());
-        list.add(2, "hello");
+
+        // ArrayList不能跨位置添加元素
+        // ArrayList<String> list = new ArrayList<>(100);
+        // System.out.println(list.size());
+        // list.add(2, "hello");
+
+        // 集合不能用null来判断，要用empty()
+        Stack<Object> stack = new Stack<>();
+        if (stack == null){
+            System.out.println("is null");
+        }else {
+            System.out.println("not null");
+        }
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(2);
+        Integer n = linkedList.removeFirst();
+        if (linkedList.size() == 0) System.out.println("is empty");
+        linkedList.add(3);
+        linkedList.add(4);
+        System.out.println(linkedList.toString());
     }
 }
