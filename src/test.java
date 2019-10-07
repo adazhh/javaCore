536f6f5d0adc0bf988d2250e52e571c1b4e02c18
pcWindows <ada.pcWindows.com>
2019-10-07 18:38:46 +0800

    add note

--- src/test.java
+++ src/test.java
@@ -4,8 +4,33 @@ import java.util.Iterator;
 
 public class test {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>(100);
-        System.out.println(list.size());
-        list.add(2, "hello");
+        // ArrayList<String> list = new ArrayList<>(100);
+        // System.out.println(list.size());
+        // list.add(2, "hello");
+
+        String a  = "CAT";
+        String b = a;
+        a = a.toLowerCase();
+        a.replace("c", "W");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+        System.out.println(a.toLowerCase());
+        System.out.println(b.replace("C", "w"));
+        System.out.println(b);
+
+        System.out.println("--------------------");
+
+        Integer i1 = 2;
+        Integer i2 = i1;
+        System.out.println(i1 == i2);
+        System.out.println(i1.equals(i2));
+        i1++;
+        System.out.println(i1 == i2);
+        System.out.println(i1.equals(i2));
+
+        System.out.println("===========");
+        Object o1 = new Object();
     }
 }
